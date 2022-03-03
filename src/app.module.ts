8bb52fa8c19376084d42932484b3d4e824e4f7cf
jbson_x240 <jb@jbson.net>
2022-03-03 11:49:58 +0100

    change config

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: ['.env.development.local'] }),
+    ConfigModule.forRoot(),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
